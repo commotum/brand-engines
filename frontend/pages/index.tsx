c8c7d73c8c110e2289d03b8d7d3feaf4900b27e9
JP <jneilpeterson@gmail.com>
2021-05-15 17:39:25 -0700

    Add rename and delete dialogs

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -7,8 +7,12 @@ import { TopPanel } from '../components/TopPanel'
 import { useModels } from '../utils/hooks/useModels'
 import { Throbber } from '../components/Throbber'
 import { ItemGrid } from '../components/ItemGrid'
+import { RenameModal } from '../components/RenameModal'
+import { useRename } from '../utils/hooks/useRename'
 import { TrainOptionModal } from '../components/TrainOptionModal'
 import { useTrainOption } from '../utils/hooks/useTrainOption'
+import { useDelete } from '../utils/hooks/useDelete'
+import { DeleteModal } from '../components/DeleteModal'
 import { ForkModal } from '../components/ForkModal'
 import { useFork } from '../utils/hooks/useFork'
 import { useTrain } from '../utils/hooks/useTrain'
@@ -36,6 +40,13 @@ const Index: NextPage<Props> = () => {
   const [sort, onSort] = React.useState<SortKey>('name')
   const { data, isLoading, isError, refresh } = useModels(search, sort)
   const {
+    id: renameId,
+    onClose: onRenameClose,
+    isOpen: isRenameOpen,
+    onRename,
+    onSubmit: onRenameSubmit,
+  } = useRename(refresh)
+  const {
     id: trainId,
     onClose: onTrainOptionClose,
     isOpen: isTrainOptionOpen,
@@ -56,6 +67,12 @@ const Index: NextPage<Props> = () => {
     onTrainOpen,
     onContinue: onTrainContinue,
   } = useTrain()
+  const {
+    onClose: onDeleteClose,
+    isOpen: isDeleteOpen,
+    onDeleteConfirm,
+    onDelete,
+  } = useDelete(refresh)
 
   const onForkModal = React.useCallback(() => {
     onForkOpen(trainId)
@@ -107,10 +124,18 @@ const Index: NextPage<Props> = () => {
     <Wrap>
       <TopPanel onSearch={onSearch} onSort={onSort} />
       <ItemGrid
+        onRename={onRename}
         onTrain={onTrain}
+        onDelete={onDelete}
         onGenerate={onGenerate}
         onHistory={onHistory}
         items={data}
+      />
+      <RenameModal
+        onClose={onRenameClose}
+        onSubmit={onRenameSubmit}
+        currentName={renameId}
+        isOpen={isRenameOpen}
       />
       <TrainOptionModal
         id={trainId}
@@ -132,6 +157,11 @@ const Index: NextPage<Props> = () => {
         onContinue={onFork}
         isLoading={isForkLoading}
         isOpen={isForkOpen}
+      />
+      <DeleteModal
+        onClose={onDeleteClose}
+        onDelete={onDeleteConfirm}
+        isOpen={isDeleteOpen}
       />
     </Wrap>
   )
